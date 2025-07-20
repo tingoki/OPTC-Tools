ceb8394c198b6e394a707deaa6936a3dbab73faf
tingoki <61174713+tingoki@users.noreply.github.com>
2025-07-20 06:01:18 -0400

    Update IconDownloadJP.py
    same comment as glb

--- IconDownloadJP.py
+++ IconDownloadJP.py
@@ -2,20 +2,27 @@ import requests
 import os
 
 # start and end number pretty obvious, remember it starts with 0001 not 1 
-start_number = ''
-end_number = ''
+start_number_str = ''
+end_number_str = ''
 
 # Use the path you wish to save icons to
 destination_folder = 'path'
 
+# Convert string numbers to integers for iteration
+start_number = int(start_number_str)
+end_number = int(end_number_str)
+
+# Determine the length for zero-padding based on the start_number_str
+padding_length = len(start_number_str)
+
 if not os.path.exists(destination_folder):
     os.makedirs(destination_folder)
 
-for number in range(int(start_number), end_number + 1):
+for number in range(start_number, end_number + 1):
    
-    number_str = str(number).zfill(len(start_number))
+    number_str = str(number).zfill(padding_length)
 
-    url = f'https://cdn.onepiece-tc.jp/images/characters/character_{number_str}_t.png'
+    url = f'https://cdn.gb.onepiece-tc.jp/images/characters/character_{number_str}_t.png'
 
     filename = f'character_{number_str}_t.png'
     destination_file = os.path.join(destination_folder, filename)
@@ -24,16 +31,20 @@ for number in range(int(start_number), end_number + 1):
         print(f"Skipped file {destination_file} (already downloaded)")
         continue
 
-    response = requests.get(url)
+    try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status() # Raise an HTTPError for bad responses (4xx or 5xx)
 
-    if response.status_code == 200:
-        try:
-            with open(destination_file, 'wb') as file:
-                file.write(response.content)
-            print(f"Image saved: {destination_file}")
-        except IOError as e:
-            print(f"Failed to save image: {e}")
-    elif response.status_code == 403:
-        print(f"Skipped file {destination_file} (Not A Unit?)")
-    else:
-        print(f"Failed to download image: {response.status_code} - {response.reason}")
+        with open(destination_file, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+        print(f"Image saved: {destination_file}")
+    except requests.exceptions.HTTPError as e:
+        if e.response.status_code == 403:
+            print(f"Skipped file {destination_file} (Not A Unit?)")
+        else:
+            print(f"Failed to download image: {e.response.status_code} - {e.response.reason}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred during the request for {destination_file}: {e}")
+    except IOError as e:
+        print(f"Failed to save image: {e}")
